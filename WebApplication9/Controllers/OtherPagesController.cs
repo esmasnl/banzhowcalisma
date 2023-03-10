249c2bb537cb16295e20d673836c80321050042e
esmasnl <ercument1649@gmail.com>
2023-03-10 09:47:21 +0300

    Add project files.

---
+++ WebApplication9/Controllers/OtherPagesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication9.Controllers
+{
+    public class OtherPagesController : Controller
+    {
+        // GET: OtherPages
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Sidebar()
+        {
+            return View();
+        }
+        public ActionResult Faq()
+        {
+            return View();
+        }
+        public ActionResult o404()
+        {
+            return View();
+        }
+    }
+}
